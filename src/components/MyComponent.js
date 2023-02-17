2d1b6010b9f10a1259f710c7cd877c91ea7ff06a
haithanh <haithanh9718@gmail.com>
2023-02-17 15:18:07 +0700

    React State

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,9 +4,17 @@ import React from "react";
 class MyComponent extends React.Component {
     //JSX cú pháp đặc biệt riêng của React
     //JSX cho phép các bạn viết javascript bên trong code html
+    state =  {
+        name :'Hai Thanh',
+        address : "HCM",
+        age: 18
+    };
     render(){
         return (
-            <div> my first component</div>
+            <div> my first component
+                My name is {this.state.name} and i'm from {this.state.address}
+
+            </div>
         );
     }
 }
