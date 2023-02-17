74cc50f35260278379999c3f9d3ab81f88df577e
haithanh <haithanh9718@gmail.com>
2023-02-17 14:34:36 +0700

    React Component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+class MyComponent extends React.Component {
+    //JSX cú pháp đặc biệt riêng của React
+    //JSX cho phép các bạn viết javascript bên trong code html
+    render(){
+        return (
+            <div> my first component</div>
+        );
+    }
+}
+
+export default MyComponent;
