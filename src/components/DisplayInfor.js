32bf1b275edf017b593c376bf91b3a17456903cc
haithanh <haithanh9718@gmail.com>
2023-02-18 10:21:08 +0700

    Conditional Output

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,19 +1,36 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  state = {
+    isShowHide: true,
+  };
+  handleShowHide = (e) => {
+    this.setState({
+      isShowHide: !this.state.isShowHide,
+    });
+  };
   render() {
     // console.log(this.props);
     const { listUsers } = this.props;
+    // console.log(listUsers);
     return (
       <div>
-        {listUsers.map((user) => {
-          return (
-            <div key={user.id}>
-              <div>My Name is {user.name}</div>
-              <div>My Age is {user.age}</div>
-            </div>
-          );
-        })}
+        <button onClick={(e) => this.handleShowHide(e)}>
+          {this.state.isShowHide === true ? "Hide" : "Show"}
+        </button>
+        {this.state.isShowHide && (
+          <div>
+            {listUsers.map((user) => {
+              // console.log(">>>Check map user: ", user);
+              return (
+                <div key={user.id} className={+user.age > 30 ? "red" : "green"}>
+                  <div>My Name is {user.name}</div>
+                  <div>My Age is {user.age}</div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
