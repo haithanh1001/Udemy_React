6e20edbf595f2f922a1fd22c39320d5ce6b2a5f3
haithanh <haithanh9718@gmail.com>
2023-02-18 09:15:15 +0700

    Outputting List

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,15 +1,22 @@
 import React from "react";
 
-class DisplayInfor extends React.Component{
-    render(){
-        let {name,age} = this.props
-        return (
-            <div>
-                <div>My Name is {name}</div>
-                <div>My Age is {age}</div>
+class DisplayInfor extends React.Component {
+  render() {
+    // console.log(this.props);
+    const { listUsers } = this.props;
+    return (
+      <div>
+        {listUsers.map((user) => {
+          return (
+            <div key={user.id}>
+              <div>My Name is {user.name}</div>
+              <div>My Age is {user.age}</div>
             </div>
-        )
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default DisplayInfor;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,11 +2,21 @@ import React from "react";
 import UserInfor from "./UserInfor";
 import DisplayInfor from "./DisplayInfor";
 class MyComponent extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: "HoidanIT", age: 30 },
+      { id: 2, name: "Bi Nguyen", age: 29 },
+      { id: 3, name: "HaryPhamDev", age: 38 },
+      { id: 4, name: "Trung Tu le", age: 19 },
+    ],
+  };
   render() {
+    const {listUsers} = this.state;
     return (
       <div>
         <UserInfor />
-        <DisplayInfor name="HoidanIT" age={30}/>
+        <hr/>
+        <DisplayInfor listUsers={listUsers} />
       </div>
     );
   }
