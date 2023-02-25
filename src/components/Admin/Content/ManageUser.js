beb93abb725120c8c94224cef52911dcc2d525f7
haithanh <haithanh9718@gmail.com>
2023-02-25 12:05:27 +0700

    State hoa' modal add new user

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,17 +1,15 @@
 import ModalCreateUser from "./ModalCreateUser";
+import "./ManageUser.scss";
 const ManageUser = (props) => {
   return (
-    <div>
-      <div className="manage-user-container">
-        <div className="title">Manage User</div>
-        <div className="users-content">
-          <div></div>
+    <div className="manage-user-container">
+      <div className="title">Manage User</div>
+      <div className="users-content">
+        <div>
           <button>Add New User</button>
         </div>
-        <div>
-          Table Users
-          <ModalCreateUser />
-        </div>
+        <div>Table Users</div>
+        <ModalCreateUser />
       </div>
     </div>
   );
