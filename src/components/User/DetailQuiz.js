1412d22ce9dd095d90dd33abb09545a46073123b
haithanh <haithanh9718@gmail.com>
2023-03-11 07:57:56 +0700

    design quiz layout

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,11 +1,15 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
+import { useLocation } from "react-router-dom";
 import _ from "lodash";
+import "./DetailQuiz.scss";
 const DetailQuiz = (props) => {
   const params = useParams();
   //   console.log(">>>check params: ", params);
   const quizId = params.id;
+  const location = useLocation();
+  console.log("Location: ", location);
   useEffect(() => {
     fetchQuestions();
   }, [quizId]);
@@ -39,7 +43,32 @@ const DetailQuiz = (props) => {
       console.log("data: ", data);
     }
   };
-  return <div className="detail-quiz-container">DetailQuiz</div>;
+  return (
+    <div className="detail-quiz-container">
+      <div className="left-content">
+        <div className="title">
+          Quiz {quizId}: {location?.state?.quizTitle}
+        </div>
+        <hr />
+        <div className="q-body">
+          <img src="" alt="" />
+        </div>
+        <div className="q-content">
+          <div className="question">Question 1: How are you doing</div>
+          <div className="answer">
+            <div className="a-child">A. Cau tra loi 1</div>
+            <div className="a-child">B. Cau tra loi 2</div>
+            <div className="a-child">C. Cau tra loi 3</div>
+          </div>
+        </div>
+        <div className="footer">
+          <button className="btn btn-secondary">Prev</button>
+          <button className="btn btn-primary">Next</button>
+        </div>
+      </div>
+      <div className="right-content">Count Down</div>
+    </div>
+  );
 };
 
 export default DetailQuiz;
